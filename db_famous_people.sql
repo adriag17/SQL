307dea5c2299070b6c66dfcedd991b442f5eeccf
A.A <57612337+adriag17@users.noreply.github.com>
2023-06-08 16:43:55 -0600

    Update db_famous_people.sql

--- db_famous_people.sql
+++ db_famous_people.sql
@@ -10,6 +10,16 @@ INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Emm
 INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Robert Pattinson", "May 13", 37, "England");
 INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("JK Rowling", "July 31", 57, "England");
 INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Quentin Tarantino", "March 27", 60, "USA");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Beyonce", "September 4", 41, "USA");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Kendrick Lamar", "June 17", 35, "USA");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Jennifer Aniston", "February 11", 54, "USA");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Daniel Craig", "March 2", 55, "England");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Adriene Mishler", "September 29", 38, "USA");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Maddie Ziegler", "September 30", 20, "USA");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Chantel Jeffries", "September 30", 30, "USA");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Martin Lawrence", "April 16", 58, "Germany");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Cole Sprouse", "August 4", 30, "Italy");
+INSERT INTO famous_people (fullname, birthdate, age, birth_country) VALUES ("Drake", "October 24", 36, "Canada");
 
 CREATE TABLE professions(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,6 +31,16 @@ INSERT INTO professions(famous_id, name) VALUES (3, "Author");
 INSERT INTO professions(famous_id, name) VALUES (2, "Actress");
 INSERT INTO professions(famous_id, name) VALUES (5, "Director");
 INSERT INTO professions(famous_id, name) VALUES (4, "Writer");
+INSERT INTO professions(famous_id, name) VALUES (6, "Singer");
+INSERT INTO professions(famous_id, name) VALUES (7, "Singer");
+INSERT INTO professions(famous_id, name) VALUES (8, "Actress");
+INSERT INTO professions(famous_id, name) VALUES (9, "Actor");
+INSERT INTO professions(famous_id, name) VALUES (10, "YouTube Star");
+INSERT INTO professions(famous_id, name) VALUES (11, "Dancer");
+INSERT INTO professions(famous_id, name) VALUES (12, "DJ");
+INSERT INTO professions(famous_id, name) VALUES (13, "YouTube Star");
+INSERT INTO professions(famous_id, name) VALUES (14, "Actor");
+INSERT INTO professions(famous_id, name) VALUES (15, "Singer");
 
 CREATE TABLE hobbies(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,8 +52,21 @@ INSERT INTO hobbies(famous_id, name) VALUES (1, "Jump rope");
 INSERT INTO hobbies(famous_id, name) VALUES (3, "Brazilian Jiujitsu");
 INSERT INTO hobbies(famous_id, name) VALUES (4, "Hiking");
 INSERT INTO hobbies(famous_id, name) VALUES (5, "Sudoku");
+INSERT INTO hobbies(famous_id, name) VALUES (6, "Dance");
+INSERT INTO hobbies(famous_id, name) VALUES (7, "Sudoku");
+INSERT INTO hobbies(famous_id, name) VALUES (9, "Play videogames");
+INSERT INTO hobbies(famous_id, name) VALUES (13, "Climbing rocks");
+INSERT INTO hobbies(famous_id, name) VALUES (12, "Archery");
+INSERT INTO hobbies(famous_id, name) VALUES (11, "Bounge");
+INSERT INTO hobbies(famous_id, name) VALUES (10, "Cooking");
+INSERT INTO hobbies(famous_id, name) VALUES (8, "Knitting");
+INSERT INTO hobbies(famous_id, name) VALUES (14, "Play bingo");
+INSERT INTO hobbies(famous_id, name) VALUES (15, "Play the guitar");
 
 /* MY PRACTICE */
+
+SELECT * 
+FROM famous_people;
 
 SELECT famous_people.fullname as "Famous born in England"
 FROM famous_people
@@ -54,16 +87,13 @@ FROM famous_people
 JOIN professions b
 ON famous_people.id = b.famous_id
 JOIN hobbies c
-ON famous_people.id = c.famous_id;
+ON famous_people.id = c.famous_id
+ORDER BY profession ASC;
 
 SELECT * 
 FROM famous_people
 WHERE age > 50;
 
-
-
-
-
-
-
-
+SELECT * 
+FROM famous_people
+WHERE age < 50;
